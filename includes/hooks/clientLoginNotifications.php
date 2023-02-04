8bfc3805305881dc765a81dce9ea7560f102018b
Jordan Schelew <jordan@websavers.ca>
2023-02-04 16:25:14 -0600

    Merge queries into one with join
    Reduce SQL queries.

--- includes/hooks/clientLoginNotifications.php
+++ includes/hooks/clientLoginNotifications.php
@@ -13,21 +13,22 @@ add_hook('UserLogin', 1, function($vars)
     if ($currentUser->isAuthenticatedAdmin()) return;
 
     $user = $vars['user'];
-    $userid = $user->id;
 
-    // auth_user_id is unique
-    $userobj = Capsule::table('tblusers_clients')->select('auth_user_id', 'client_id', 'owner')->where('auth_user_id', '=', $userid)->first();
-    $clientid = $userobj->client_id;
+    $userobj = Capsule::table('tblusers_clients')
+        ->join('tblusers', 'tblusers_clients.auth_user_id', '=', 'tblusers.id')
+        ->select('tblusers_clients.client_id', 'tblusers_clients.owner', 'tblusers.first_name', 'tblusers.last_name', 'tblusers.last_ip')
+        ->where('tblusers_clients.auth_user_id', '=', $user->id)
+        ->first();
 
-    // userid is unique
-    $userdata = Capsule::table('tblusers')->select('first_name', 'last_name', 'email', 'last_ip')->where('id', $userid)->first();
-    $firstname = $userdata->first_name;
-    $lastname = $userdata->last_name;
-    $email = $userdata->email;
+    $clientid   = $userobj->client_id;
+    $firstname  = $userobj->first_name;
+    $lastname   = $userobj->last_name;
+    $email      = $userobj->email;
+    $last_ip    = $userobj->last_ip;
 
     $ip = $_SERVER['REMOTE_ADDR'];
 
-    if ($userdata->last_ip === $ip){ 
+    if ($last_ip === $ip){ 
         return; //Don't alert if user is logging in from the usual IP
     }
 
@@ -37,7 +38,7 @@ add_hook('UserLogin', 1, function($vars)
     
     $subject = "User Login from new IP $ip ($hostname)";
     
-    $message = "<p>A user just logged in to your account from a different IP than usual:</p>
+    $message = "<p>A user just accessed your account from a different IP than usual:</p>
         <ul>
             <li>Name: $firstname $lastname</li>
             <li>Email: $email</li>
@@ -47,7 +48,7 @@ add_hook('UserLogin', 1, function($vars)
         </ul>
         <p>You can manage the users allowed to access your account here: {$GLOBALS['CONFIG']['SystemURL']}/account/users.</p>";
 
-    $results = localAPI('sendemail', array(
+    $results = localAPI('SendEmail', array(
         'customtype'        => 'general',
         'customsubject'     => $subject,
         'custommessage'     => $message,
